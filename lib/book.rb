ec24ae406738f0cf562041f3207da4fd0d09fd63
meganletendre <megan.letendre@comcast.net>
2018-07-13 14:26:02 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,18 +1,18 @@
-# # book.rb
-# # Add you Book class here
-# class Book 
+# book.rb
+# Add you Book class here
+class Book 
   
-#   attr_reader :title
-#   attr_accessor :author, :genre, :page_count
+  attr_reader :title
+  attr_accessor :author, :genre, :page_count
   
-#   def initialize(title)
-#     @title = title 
-#   end
-#   def turn_page
-#     puts "Flipping the page...wow, you read fast!"
-#   end 
+  def initialize(title)
+    @title = title 
+  end
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end 
 
-# end
+end
 
-# great_gatsby =  Book.new("The Great Gatsby")
-# puts great_gatsby.title
+great_gatsby =  Book.new("The Great Gatsby")
+puts great_gatsby.title
