25bfdd11174aea0fe21a129de621f59e1b9d22fa
meganletendre <megan.letendre@comcast.net>
2018-07-13 14:25:26 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,11 @@
 # Make your shoe class here!
+
+class Shoe
+  attr_reader :brand
+  attr_accessor :color,:size,:material,:condition
+  
+  def initialize(brand)
+    @brand = brand
+  end
+end
+adidas = Shoe.new("Adidas")
